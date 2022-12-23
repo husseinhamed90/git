f33c1e0013d6fdbfe0d44d54117afb79142e5156
Hussein Hamed <81229455+husseinhamed90@users.noreply.github.com>
2022-12-23 22:11:45 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,6 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
-  // home from hussein
-  // home 20000000000001111111111111111111111110
-  // home 12121
-  // home 20
-  // huhuhuhuhuhuhuhuhuhuhuhuhuhuhuhuhuhuhuh
-  // home 20000000000000000000000000000000000
-  // from android not console 6800000000000009
 
   @override
   Widget build(BuildContext context) {
@@ -23,16 +16,8 @@ class MyApp extends StatelessWidget {
       debugShowMaterialGrid: false,
       title: 'hussein',
       theme: ThemeData(
-        backgroundColor: Colors.black,
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        backgroundColor: Colors.red,
+ 
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
